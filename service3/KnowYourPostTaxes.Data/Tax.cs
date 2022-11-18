f75520202a16ef67c7812001753b453d62cdcbd7
AndriiZubrey <76507660+AndriiZubrey@users.noreply.github.com>
2022-11-18 07:39:11 +0200

    Update Tax.cs

--- service3/KnowYourPostTaxes.Data/Tax.cs
+++ service3/KnowYourPostTaxes.Data/Tax.cs
@@ -8,4 +8,10 @@ public class Tax
     [Key] [DatabaseGenerated(DatabaseGeneratedOption.Identity)] public int Id { get; set; }
     public string Name { get; set; }
     public decimal(4, 3) TaxRate { get; set; } 
+    
+    public Tax(string name, decimal(4, 3) taxrate)
+    {
+        Name = name;
+        TaxRate = taxrate;
+    }
 }
