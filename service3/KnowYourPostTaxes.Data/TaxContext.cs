53defc37d2e7b36c65ea890f521c5d1d844f480a
AndriiZubrey <76507660+AndriiZubrey@users.noreply.github.com>
2022-11-18 07:51:15 +0200

    Database Redo

--- service3/KnowYourPostTaxes.Data/TaxContext.cs
+++ service3/KnowYourPostTaxes.Data/TaxContext.cs
@@ -1,24 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace KnowYourPostTaxes.Data;
 
 public class TaxContext : DbContext
 {
-    public DbSet<Tax> Taxes { get; set; }
-    
+    public DbSet<Tax> Users { get; set; }
+
     public TaxContext(DbContextOptions<TaxContext> options) 
         : base(options)
     {
     }
-    
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-    }
-    
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<Tax>()
-            .HasIndex(e => e.Email)
-            .IsUnique();
     }
 }
