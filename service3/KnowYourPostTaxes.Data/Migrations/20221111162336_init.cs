406853d6e8af0c8c6f112e5c62fa05867f763a18
AndriiZubrey <76507660+AndriiZubrey@users.noreply.github.com>
2022-11-18 06:48:49 +0200

    Database Redo

--- service3/KnowYourPostTaxes.Data/Migrations/20221111162336_init.cs
+++ service3/KnowYourPostTaxes.Data/Migrations/20221111162336_init.cs
@@ -16,19 +16,13 @@ namespace KnowYourPostTaxes.Data.Migrations
                     Id = table.Column<int>(type: "integer", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
                     Name = table.Column<string>(type: "text", nullable: false),
-                    Email = table.Column<string>(type: "text", nullable: false),
-                    Password = table.Column<string>(type: "text", nullable: false)
+                    TaxRate = table.Column<string>(type: "decimal(4, 3)", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("PK_Taxes", x => x.Id);
                 });
 
-            migrationBuilder.CreateIndex(
-                name: "IX_Taxes_Email",
-                table: "Taxes",
-                column: "Email",
-                unique: true);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
